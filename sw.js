73bbd5ce30a6264cc7a18ccea1590e296f478e18
Ignacio <dev.ignacio.h@gmail.com>
2021-02-05 13:31:26 -0300

    instalador

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cuando se instala el service worker
-// buen lugar para cachear ciertos archivos
+// Buen lugar para cachear ciertos archivos
 self.addEventListener('install', e => {
     console.log('Instalado el service worker');
 
@@ -7,9 +7,14 @@ self.addEventListener('install', e => {
 })
 
 // Activar el Service worker
-// buen lugar para nuevas versiones de la PWA
+// Buen lugar para nuevas versiones de la PWA
 self.addEventListener('activate', e => {
     console.log('Service Worker Activado');
 
     console.log(e);
+})
+
+// Evento fetch para descargar archivos estáticos
+self.addEventListener('fetch', e => {
+    console.log(('Fetch...', e));
 })
