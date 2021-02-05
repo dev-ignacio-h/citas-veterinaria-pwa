f462f25851cc95b4370af98b7709952582f239da
Ignacio <dev.ignacio.h@gmail.com>
2021-02-05 15:53:38 -0300

    soporte offline

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
 // archivos a cachear
-const nombreCache = 'apv-v1';
+const nombreCache = "apv-v1";
 const archivos = [
-  '/',
-  '/index.html',
-  '/css/bootstrap.css',
-  '/css/styles.css',
-  '/js/app.js',
-  '/js/apv.js'
+  "/",
+  "/index.html",
+  "/css/bootstrap.css",
+  "/css/styles.css",
+  "/js/app.js",
+  "/js/apv.js",
 ];
 // Cuando se instala el service worker
 // Buen lugar para cachear ciertos archivos
-self.addEventListener('install', (e) => {
-  console.log('Instalado el service worker');
+self.addEventListener("install", (e) => {
+  console.log("Instalado el service worker");
 
   // Se espera a que se descarguen todos los archivos de caché y se agregan al caché
   e.waitUntil(
     caches.open(nombreCache).then((cache) => {
-      console.log('cacheando');
+      console.log("cacheando");
       cache.addAll(archivos); // si es sólo un archivo se puede usar .add
     })
   );
@@ -24,13 +24,18 @@ self.addEventListener('install', (e) => {
 
 // Activar el Service worker
 // Buen lugar para nuevas versiones de la PWA
-self.addEventListener('activate', (e) => {
-  console.log('Service Worker Activado');
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker Activado");
 
   console.log(e);
 });
 
 // Evento fetch para descargar archivos estáticos
-self.addEventListener('fetch', (e) => {
-  console.log(('Fetch...', e));
+self.addEventListener("fetch", (e) => {
+  console.log(("Fetch...", e));
+  e.respondWith(
+    caches.match(e.request).then((respCache) => {
+      return respCache;
+    })
+  );
 });
